9d3109ca9bb0930073e659518cb06f2a2e81304a
Administrator <Administrator@RJWHBSYJFXIZX5E>
2016-08-01 16:48:53 +0800

    2016.8.1PM

--- MusicStore/MusicStore/Controllers/HomeController.cs
+++ MusicStore/MusicStore/Controllers/HomeController.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MusicStore.Models;
 
 namespace MusicStore.Controllers
 {
     public class HomeController : Controller
     {
+        MusicStoreEntity db = new MusicStoreEntity();
         public ActionResult Index()
         {
-            return View();
+            
+            List<Albums> list= db.Albums.OrderByDescending(a=>a.OrderDetails.Count).Take(12).ToList();
+            return View(list);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Albums a = db.Albums.Find(id);
+            return View(a);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
---
+++ MusicStore/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+using X.PagedList;
+
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntity db = new MusicStoreEntity();
+        // GET: Store
+        public ActionResult Index(string search,int page=1)
+        {
+            var pageSize = 24;
+            IPagedList<Albums> list = null;
+            if (string.IsNullOrEmpty(search))
+            {
+                list = db.Albums.OrderByDescending(a => a.AlbumId).ToPagedList(page, pageSize);
+            }
+            else
+            {
+                ViewBag.search = search;
+                list = db.Albums.Where(a=>a.Title.Contains(search)).OrderByDescending(a => a.AlbumId).ToPagedList(page, pageSize);
+            }                       
+            
+            return View(list);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Albums a=db.Albums.Find(id);
+            return View(a);
+        }
+    }
+}
